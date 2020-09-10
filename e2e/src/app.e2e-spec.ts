e888be6d8c6d012148490c3f533f33b8b992708f
Milo Tomáš <milot@softec.sk>
2020-09-10 07:12:01 +0200

    renaming

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('TomasMiloTemplate app is running!');
+    expect(page.getTitleText()).toEqual('TomasMilo app is running!');
   });
 
   afterEach(async () => {
